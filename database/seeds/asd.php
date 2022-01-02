4fbe58b759bf17fc9e79a72beba959aa91f70f02
haryosuryantoko <haryosuryantoko@gmail.com>
2022-01-02 13:16:14 +0700

    1

---
+++ database/seeds/asd.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class asd extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        // DB::table('users')->insert([
+        //     'code'              => '1001',
+        //     'first_name'        => 'Damien',
+        //     'last_name'         => 'Martin',
+        //     'email'             => 'admin@demo.com',
+        //     'password'          => bcrypt('123456'),
+        //     'is_administrator'  => TRUE, // Administrator
+        // ]);
+    }
+}
